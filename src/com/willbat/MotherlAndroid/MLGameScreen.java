27dc7e8731d53129f91bee21fafe98f073014210
DaJobat <j.d.jacob.dennis@gmail.com>
2013-08-20 22:23:53 +0100

    sprites should be constructed properly in the tile constructor now textures are taken from a tilesheet needs testing updated TODO and planned features, created a new gamescreen with no functionality added a template for the tilesheet.

---
+++ src/com/willbat/MotherlAndroid/MLGameScreen.java
@@ -0,0 +1,51 @@
+package com.willbat.MotherlAndroid;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Jobat
+ * Date: 20/08/13
+ * Time: 21:45
+ * To change this template use File | Settings | File Templates.
+ */
+public class MLGameScreen implements Screen {
+    @Override
+    public void render(float delta) {
+        //To change body of implemented methods use File | Settings | File Templates.
+        Gdx.gl20.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        Gdx.gl20.glClearColor(100.0F, 149.0F, 237.0F, 1.0F);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void show() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void hide() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void pause() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void resume() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void dispose() {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+}
--- src/com/willbat/MotherlAndroid/Tile.java
+++ src/com/willbat/MotherlAndroid/Tile.java
@@ -1,21 +1,39 @@
 package com.willbat.MotherlAndroid;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+import java.util.Random;
 
 /**
  * This class contains all the properties of a tile.
  */
 public class Tile {
-    private Texture texture;
-    private int textureLocation;
+    protected Sprite tileSprite;
+    protected Texture texture;
     protected int mineLevel;
     protected boolean visible;
+    protected int[] location;
 
-    public Tile(Tiletype type) {
-        for (Tiletype properties : type.values()) {
-            mineLevel = properties.mineLevel;
-            textureLocation = properties.textureLocation;
+    public Tile(Tiletype type, int x, int y) {
+        // Constructor for each tile.
+        texture = new Texture("res/tilesheettemplate.png");
+        int[] texLocation =  getTexture(type.textureLocation);
+        tileSprite = new Sprite(texture,texLocation[0], texLocation[1],32,32);
+        location = new int[]{x,y};
+        mineLevel = type.mineLevel;
+    }
 
+    private int[] getTexture(int location) {
+        //result contains the x and y location of the texture in the texture sheet.
+        int[] result = {location*32,0};
+        // this gets a random texture from the correct column on the tilesheet
+        int altTiles = ((texture.getHeight()/32) - 1);
+        if (altTiles > 0)
+        {
+            Random tilepicker = new Random();
+            result[1] = tilepicker.nextInt(altTiles)*32;
         }
+        return result;
     }
 }
